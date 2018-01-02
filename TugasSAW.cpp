a248471292eaa9536919f51357285ae886164240
AhmadSyaifuddin <syaifuddinahmad598@gmail.com>
2018-01-02 15:27:30 +0800

    Menambahkan Informasi Koding sebelumnya yang digunakan untuk menampilakan Nilai akhir
    Menambahkan Informasi Koding sebelumnya yang digunakan untuk menampilakan Nilai akhir

--- TugasSAW.cpp
+++ TugasSAW.cpp
@@ -92,8 +92,8 @@ int main(){
 		}while(r<5);cout << endl;
 		
 	cout << endl;
-	cout << "NILAI AKHIR:   " << endl;
-	cout <<"Nilai A1   :   "<<ha[0] << endl;
-	cout <<"Nilai A2   :   "<<ha[1] << endl;
-	cout <<"Nilai A3   :   "<<ha[2] << endl;
+		cout << "NILAI AKHIR:   " << endl;
+		cout <<"Nilai A1   :   "<<ha[0] << endl;
+		cout <<"Nilai A2   :   "<<ha[1] << endl;
+		cout <<"Nilai A3   :   "<<ha[2] << endl;
 }
